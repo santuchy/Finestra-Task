a6ac9b12711e70efd67d9d9c5975604b10873ef3
Santuchy <santuchowdhuryofficial@gmail.com>
2025-11-18 00:38:10 +0600

    basic route setup done

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/routes/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+
+
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <HomeLayout></HomeLayout>
+        }
+    ]
+);
+
+
+export default router;
